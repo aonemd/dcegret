5a64dbb4174c1674c7861bceb53ba1a39746af1f
aonemd <aonemdsaleh@gmail.com>
2019-04-17 17:25:16 +0200

    Add PostEditor component

--- frontend/components/Posts.vue
+++ frontend/components/Posts.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
-    <div v-show="authenticated" id="posts__add-post-form">
-      <textarea placeholder="Post something..." v-model="postContent"></textarea>
-      <button v-on:click="addNewPost">Add</button>
-    </div>
+  <div class="row">
+    <div class="col-4 col-m-6 center">
+      <div v-show="authenticated" id="posts__add-post-form">
+        <post-editor
+          :max-characters="180"
+          rows="4"
+          v-model="postContent">
+        </post-editor>
 
-    <div v-for="post in posts">
-      <div class="post">
-        <h1>
-          {{ post.content }}
-        </h1>
+        <button v-on:click="addNewPost">Add</button>
+      </div>
+
+      <div v-for="post in posts">
+        <div class="post">
+          <h1>
+            {{ post.content }}
+          </h1>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import api from '../services/api/posts.js'
+import api from '../services/api/posts.js';
+import PostEditor from './posts/PostEditor';
 
 export default {
+  components: {
+    'post-editor': PostEditor
+  },
   data() {
     return {
       posts: [],
@@ -37,6 +48,7 @@ export default {
     addNewPost: function () {
       api.create(this.postContent, this.$store.getters.current_account_token).then((response) => {
         this.posts.unshift({ content: this.postContent });
+        this.postContent = '';
       });
     }
   }
---
+++ frontend/components/posts/PostEditor.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <textarea
+      id="post__editor"
+      v-model='taValue'
+      cols="200"
+      :rows='rows'
+      @input='$emit("input", taValue)'
+      placeholder="Chirp something...">
+    </textarea>
+
+    <label
+      id="post__editor-header"
+      :class="[{'post___over-max': isOverMax}, 'text-right']"
+      for="post__editor">
+      {{ remaining }} characters left
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["maxCharacters", "rows", "value"],
+  data(){
+    return {
+      taValue: this.value
+    }
+  },
+  computed:{
+    isOverMax(){
+      if (!this.taValue)
+        return false;
+
+      return (this.maxCharacters - this.taValue.length) < 0;
+    },
+    remaining(){
+      if (!this.taValue)
+        return this.maxCharacters;
+
+      return this.maxCharacters - this.taValue.length
+    }
+  }
+}
+</script>
+
+<style scoped>
+textarea {
+  display: block;
+  resize: none;
+  outline: none;
+  width: 100%;
+  padding: 15px;
+}
+
+#post__editor-header {
+  display: block;
+  width: 100%;
+  padding-right: 10px;
+  color: white;
+  background-color: #211f1f;
+  font-size: 18px;
+}
+
+.post___over-max {
+  color: #f22c40 !important;
+}
+</style>
