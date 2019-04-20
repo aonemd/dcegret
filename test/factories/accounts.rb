049319d9eac12f7aebcb20c3471c64d7abc1b04d
aonemd <aonemdsaleh@gmail.com>
2019-04-21 00:42:18 +0200

    Set the correct password attribute name for account factory

--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :account do
     email { "#{Faker::Internet.user_name}@random.com" }
     username { Faker::Internet.user_name }
-    password_digest { "12345678" }
+    password { "12345678" }
   end
 end
