ae8082c9b08334b6443be1e2da37b0570b4ae0f0
aonemd <aonemdsaleh@gmail.com>
2019-04-30 21:19:24 +0200

    Fix PostDecorator having options as nil

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,12 +2,17 @@ class PostDecorator < Geckorate::Decorator
   include ActionView::Helpers::DateHelper
 
   def decorate(options = {})
-    {
+    out = {
       id: id,
       content: content,
       account: AccountDecorator.new(account).decorate,
-      like_id: likes.find_by(account_id: options[:current_account].id).try(:id),
       created_at: time_ago_in_words(created_at)
-    }.as_json
+    }
+
+    if options[:current_account]
+      out[:like_id] = likes.find_by(account_id: options[:current_account].id).try(:id)
+    end
+
+    out.as_json
   end
 end
